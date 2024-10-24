b8b5988671392b8e62a7cde9d6763aa8e5ca33f0
EMe-U <e.umurerwa@alustudent>
2024-10-24 11:57:02 +0000

    count

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,20 +1,24 @@
 #!/usr/bin/python3
-"""Script that fetches 10 hot posts for a given subreddit."""
+"""Script that fetch 10 hot post for a given subreddit."""
 import requests
 
 
 def top_ten(subreddit):
-    """Fetch and print the titles of the first 10 hot posts from a subreddit."""
+    """Return number of subscribers if @subreddit is valid subreddit.
+    if not return 0."""
+
     headers = {'User-Agent': 'MyAPI/0.0.1'}
-    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
-    response = requests.get(url, headers=headers, allow_redirects=False)
+    subreddit_url = "https://reddit.com/r/{}.json".format(subreddit)
+    response = requests.get(subreddit_url, headers=headers)
 
     if response.status_code == 200:
-        json_data = response.json().get("data", {})
-        children = json_data.get("children", [])
-        if not children:
-            print(None)
-        for post in children:
-            print(post.get("data", {}).get("title", ""))
+        json_data = response.json()
+        for i in range(10):
+            print(
+                json_data.get('data')
+                .get('children')[i]
+                .get('data')
+                .get('title')
+            )
     else:
         print(None)
